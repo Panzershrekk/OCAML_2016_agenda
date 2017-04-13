6f84e8d61c7d0bf8a31567b07486eec3a96804f7
Fossaert Thomas <thomas.fossaert@epitech.eu>
2017-04-13 09:45:44 +0200

    Remove

--- agenda.ml
+++ agenda.ml
@@ -1,12 +1,11 @@
-(*open Contact;;*)
 type field = All | Id | FirstName | LastName | Age | Email | Phone;;
 
 module type AGENDA =
   sig
     val addContact     : Contact.contact list -> string * string * int * string * string -> Contact.contact list
-    (*val getContactId   : Contact.contact list -> field -> string -> int
+    (*val getContactId   : Contact.contact list -> field -> string -> int*)
     val removeContact  : Contact.contact list -> int -> Contact.contact list
-    val replaceContact : Contact.contact list -> int -> string * string * int * string * string -> Contact.contact list*)
+    (*val replaceContact : Contact.contact list -> int -> string * string * int * string * string -> Contact.contact list*)
     val printContacts  : Contact.contact list -> field -> string -> unit
   end
 
@@ -15,16 +14,29 @@ module Agenda : AGENDA =
     let addContact l c =
       let newco = [Contact.createTu c] in List.append l newco;;
 
-    (*let printContacts l f s = match f with
-      | All -> Printf.printf("bonjour")
-      | Id -> Printf.printf("id")
-      | FirstName -> Printf.printf("fn")
-      | LastName -> Printf.printf("ln")
-      | Age -> Printf.printf("ag")
-      | Email -> Printf.printf("mail")
-      | Phone -> Printf.printf("phone")*)
+    (*let getContactId l f s =
+      let rec aux i = function
+        | [] -> -1
+        | head::tail -> match f with
+                        | FirstName -> if String.compare s (Contact.getFn head) = 0 then i else -1(*head::aux (i+1) tail*)
+                        | LastName -> if String.compare s (Contact.getFn head) = 0 then i else -1(*head::aux (i+1) t*)
+                        | Age -> if String.compare s (Contact.getFn head) = 0 then i else -1(*head::aux (i+1) t*)
+                        | Email -> if String.compare s (Contact.getFn head) = 0 then i else -1(*head::aux (i+1) t*)
+                        | Phone -> if String.compare s (Contact.getFn head) = 0 then i else -1(*head::aux (i+1) t*)
+      in aux i l;;*)
 
-      let rec printContacts l f s = match l with
-        | head::tail -> Printf.printf "%s\n" (Contact.getLn head) ; printContacts tail f s
-        | [] -> Printf.printf "Done\n"
+      let rec removeContact l n = match l with
+       | [] -> []
+       | head::tail -> if n = 0 then tail else head::removeContact tail (n-1);;
+
+    let rec printContacts l f s = match l with
+      | [] -> Printf.printf "\n"
+      | head::tail -> match f with
+                      | All -> Printf.printf "%i %s %s %i %s %s\n" 0 (Contact.getLn head) (Contact.getFn head) (Contact.getAge head) (Contact.getMail head) (Contact.getNb head); printContacts tail f s
+                      | Id -> Printf.printf "%s\n" (Contact.getLn head) ; printContacts tail f s
+                      | FirstName -> Printf.printf "%s\n" (Contact.getFn head) ; printContacts tail f s
+                      | LastName -> Printf.printf "%s\n" (Contact.getLn head) ; printContacts tail f s
+                      | Age -> Printf.printf "%i\n" (Contact.getAge head) ; printContacts tail f s
+                      | Email -> Printf.printf "%s\n" (Contact.getMail head) ; printContacts tail f s
+                      | Phone -> Printf.printf "%s\n" (Contact.getNb head) ; printContacts tail f s;;
   end;;
--- main.ml
+++ main.ml
@@ -1,4 +1,5 @@
-open Agenda
+open Agenda;;
+open Contact;;
 
 let c = Contact.create "Guillaume" "Collet" 25 "guillaume.collet@epitech.eu" "01 84 07 42 10";;
 
@@ -6,5 +7,18 @@ let d = Contact.createTu ("Mecasse", "lescouilles", 36, "mecass.lescouilles@epit
 let data = [c];;
 
 let one_data = Agenda.addContact data ("Mecasse", "lescouilles", 36, "mecass.lescouilles@epitech.eu", "OSEF");;
+let one_data = Agenda.addContact one_data ("billy", "billy", 36, "billy@epitech.eu", "OSEF");;
+let one_data = Agenda.addContact one_data ("poil", "liop", 36, "poil.liop@epitech.eu", "09 09 09 09 08 12");;
 
-Agenda.printContacts one_data LastName "s";;
+Agenda.printContacts one_data All "s";;
+Printf.printf "\n";;
+
+let one_data = Agenda.removeContact one_data 1;;
+
+(*Agenda.printContacts one_data LastName "s";;
+Agenda.printContacts one_data Age "s";;
+Agenda.printContacts one_data FirstName "s";;
+Agenda.printContacts one_data Email "s";;
+Agenda.printContacts one_data Phone "s";;*)
+
+Agenda.printContacts one_data All "s";;
